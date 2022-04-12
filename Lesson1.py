c8c7d1ac46d06c789c3b3e7f3a189dc4e9e8f193
*Albina <*Albinagal1312@gmail.com*>
2022-04-12 08:51:11 -0500

    Lesson1.py file got added

---
+++ Lesson1.py
@@ -0,0 +1,13 @@
+
+
+my_string = "Test Automation"
+word_test = "Test"
+print(word_test)
+
+my_string = "Test Automation"
+second_word = "Automation"
+print(second_word)
+
+my_list = [10,50,20,25,30]
+sliced_list="50,20"
+print(sliced_list)
